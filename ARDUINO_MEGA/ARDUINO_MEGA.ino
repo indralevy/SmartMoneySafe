c1309b51170795cb916e672d95a5fd643b92483a
indralevy <38361129+indralevy@users.noreply.github.com>
2019-07-07 04:52:43 +0700

    Add files via upload

---
+++ ARDUINO_MEGA/ARDUINO_MEGA.ino
@@ -0,0 +1,242 @@
+#include<SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display 
+
+int s0 = 4;
+int s1 = 5;
+int s2 = 6;
+int s3 = 7;
+int out=8;
+int enB = 9;
+int in3 = 10;//high
+int in4 = 11;//low
+int button=12;
+int led = 13;
+int red = 0;
+int green = 0;
+int blue = 0;
+int total = 0;
+int jml = 0;
+
+//int dataduit=0; 
+void setup() {
+   Serial.begin(115200);
+   lcd.init();
+   lcd.setCursor(1,0);
+   lcd.print("SMART MONEY");
+   lcd.setCursor(1,1);
+  lcd.print("SAFE Mark-I");
+  lcd.clear();
+  delay(5000);
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+  pinMode(out, INPUT);
+  pinMode(button, INPUT);
+  pinMode(led, OUTPUT);
+  digitalWrite(s0, HIGH);
+  digitalWrite(s1, HIGH);
+  digitalWrite(button, HIGH);
+}
+
+void loop()
+{
+  lcd.setCursor(1,0);
+  lcd.print(total);
+  //while(digitalRead(button)) 
+  color();
+  Serial.print("R Intensity:");
+  Serial.print(red, DEC);
+  Serial.print(" G Intensity: ");
+  Serial.print(green, DEC);
+  Serial.print(" B Intensity : ");
+  Serial.print(blue, DEC);
+  
+  Serial.println();
+  
+//  if (red < 55 && red > 32 && green < 66 && green > 47 && blue < 38 && blue > 25) 
+ 
+     if (red == 20 && green == 25 && blue == 16) 
+     {
+    //dataduit=10000;
+    Serial.write(45);
+    Serial.write("Z");
+    digitalWrite(led, HIGH);
+    jml=10;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("SEPULUH RIBU");
+    Serial.println(" - (10 Ribu)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000); 
+  }
+//
+//  
+// //else if (red == 33 && green == 38 && blue == 33) 
+// //red < 50 && red > 40 && green < 92 && green > 50 && blue < 76 && blue > 48
+    else if (red == 15 && green == 22 && blue == 20)
+  {
+//    dataduit=5000;
+//    s.write(dataduit);
+    Serial.write(45);
+    Serial.write("X");
+    digitalWrite(led, HIGH);
+    jml=5;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("LIMA RIBU");
+    Serial.println(" - (lima Ribu)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000); 
+    }
+
+
+  else if (red == 20 && green == 22 && blue == 17)
+  {
+//    dataduit=2000;
+//    s.write(dataduit);
+    Serial.write(45);
+    Serial.write("Q");
+    digitalWrite(led, HIGH);
+    jml=2;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("DUA RIBU");
+    Serial.println(" - (Dua Ribu)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000); 
+    }
+
+else if (red == 19 && green == 23 && blue == 20)
+  {
+//    dataduit=1000;
+//    s.write(dataduit);
+    Serial.write(45);
+    Serial.write("D");
+    digitalWrite(led, HIGH);
+    jml=1;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("SERIBU");
+    Serial.println(" - (Se Ribu)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000); 
+    }
+
+if (red == 27 && green == 23 && blue == 15)
+  {
+//    dataduit=50000;
+//    s.write(dataduit);
+    Serial.write(45);
+    Serial.write("E");
+    digitalWrite(led, HIGH);
+    jml=50;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("LIMA PULUH RIBU");
+    Serial.println(" - (LIMA PULUH RIBU)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000); 
+    }
+
+if (red == 14 && green == 22 && blue == 16)
+  {
+//    dataduit=100000;
+//    s.write(dataduit);
+    Serial.write(45);
+    Serial.write("F");
+    digitalWrite(led, HIGH);
+    jml=100;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("Srts Ribu");
+    Serial.println(" - (Srts Ribu)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000); 
+    }
+
+
+//else if (red < 78 && red > 55 && green < 63 && green > 61 && blue < 55 && blue > 49)
+//  else if (red == 26 && green == 25 && blue == 22)
+   if (red == 23 && green == 22 && blue == 20)
+  {
+    Serial.write(45);
+    Serial.write("g");
+    digitalWrite(led, HIGH);
+    jml=20;
+    total=jml+total;
+    lcd.clear();
+    lcd.setCursor(1,0);
+    lcd.print(total);
+    lcd.setCursor(1,1);
+    lcd.print("LIMA RIBU");
+    Serial.println(" - (lima Ribu)");
+    Serial.println(total);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    analogWrite(enB, 255);
+    delay(5000);     }
+
+  else {
+    Serial.println("COBA LAGI");
+  }
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  digitalWrite(led, LOW);   
+  delay(500);
+}
+
+
+void color()
+{
+digitalWrite(s2, LOW);
+digitalWrite(s3, LOW);
+//count OUT, pRed, RED
+red = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+digitalWrite(s3, HIGH);
+//count OUT, pBLUE, BLUE
+blue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+digitalWrite(s2, HIGH);
+//count OUT, pGreen, GREEN
+green = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+}
+
+
+
---
+++ ESP8266_Gmail_Sender/ESP8266_Gmail_Sender.ino
@@ -0,0 +1,172 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include "Gsender.h"
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> 
+#include <SoftwareSerial.h>
+#pragma region Globals
+uint8_t connection_state = 0;                    // Connected to WIFI or not
+//uint16_t reconnect_interval = 10000;             // If not connected wait time to try again
+#pragma endregion Globals
+char s = Serial.read();
+String subject;
+Gsender *gsender = Gsender::Instance();    // Getting pointer to class instance
+WiFiServer server(80);
+String header;
+WiFiClient espClient;
+int dataduit;
+
+ 
+void setup()
+{
+    Serial.begin(115200);
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    wifiManager.autoConnect("Smart Money Safe");
+//    Serial.println("Connected.");
+//    Serial.println("");
+//    Serial.println("WiFi Connected.");
+//    Serial.print("IP address: ");
+//    Serial.println(WiFi.localIP());
+//    Serial.println("");
+//    Serial.println("WiFi Connected.");
+//    Serial.print("IP address: ");
+//    Serial.println(WiFi.localIP());
+//    Serial.println("Sensor Active");
+    connection_state = WiFi.localIP();
+    
+    if(!connection_state)  // if not connected to WIFI
+        Serial.println("GAK");
+    //Gsender *gsender = Gsender::Instance();    // Getting pointer to class instance
+    subject = "SELAMAT DATANG DI SMART MONEY SAVE";
+    if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Semua data uang yang masuk akan masuk ke email ini")) {
+    
+// Serial.println("Message send.");
+    }
+    else {
+//        Serial.print("Error sending message: ");
+        Serial.println(gsender->getError());
+    }
+}
+
+void loop(){
+if (Serial.available())
+    {
+        kirim();
+    }
+WiFiClient client = server.available();
+if (client) {                             
+//    Serial.println("New Client.");          
+    String currentLine = "";                
+    while (client.connected()) {            
+      if (client.available()) {             
+        char c = client.read();             
+        //Serial.write(c);                    
+        header += c;
+        if (c == '\n') {                    
+            if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            // Web Page Heading
+            client.println("<body><h1>Smart Money Safe Link</h1>");
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+ }
+
+}
+
+void kirim(){
+    if (s == 'A'){
+    Serial.println("10K");
+//    subject = "UANG 10000 TELAH MASUK";
+//    if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Semua data uang yang masuk akan masuk ke email ini")) {
+//    Serial.println("Message send.");
+//    }
+  delay(5000);
+  }
+  
+  if(s == 'B'){
+  Serial.println("5K");
+//   subject = "UANG 5000 TELAH MASUK";
+//   if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Uang 5000 Telah Masuk")) {
+//   Serial.println("Message send.");
+//   }
+  delay(5000);
+  }
+
+  if(s == 'C'){
+  Serial.println("2K");
+//   subject = "UANG 2000 TELAH MASUK";
+//   if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Uang 2000 Telah Masuk")) {
+//   Serial.println("Message send.");
+//   }
+  delay(5000);
+  }
+
+  if(s == 'D'){
+  Serial.println("1K");
+//   subject = "UANG 1000 TELAH MASUK";
+//   if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Uang 1000 Telah Masuk")) {
+//   Serial.println("Message send.");
+//   }
+  delay(5000);
+  }
+
+  if(s == 'E'){
+  Serial.println("50K");
+//   subject = "UANG 50000 TELAH MASUK";
+//   if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Uang 50000 Telah Masuk")) {
+//   Serial.println("Message send.");
+//    }
+  delay(5000);
+  }
+  
+  if(s == 'F'){
+  Serial.println("100K");
+//  subject = "UANG 100000 TELAH MASUK";
+//  if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Uang 100000 Telah Masuk")) {
+//  Serial.println("Message send.");
+//  }
+ delay(5000);
+
+ }
+
+if(s == 'g'){
+  Serial.println("20K");
+//  subject = "UANG 100000 TELAH MASUK";
+//  if(gsender->Subject(subject)->Send("indrajitalepro@gmail.com", "Uang 100000 Telah Masuk")) {
+//  Serial.println("Message send.");
+//  }
+ delay(5000);
+
+ }
+}
+
